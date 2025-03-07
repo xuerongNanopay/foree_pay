d43d063b15ab7ecb0f0d871677f84704bb1d64af
xuerongNanopay <xuerong@nanopay.net>
2025-03-06 22:56:55 -0500

    save change

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 #![allow(unused)]
 
+use foam_rust::GettersSetters;
+
+#[derive(GettersSetters)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut person = Person {
+        name: "Alice".to_string(),
+        age: 25,
+    };
+
+    println!("Name: {}", person.get_name());
+    println!("Age: {}", person.get_age());
+
+    person.set_name("Bob".to_string());
+    person.set_age(30);
+
+    println!("Updated Name: {}", person.get_name());
+    println!("Updated Age: {}", person.get_age());
 }

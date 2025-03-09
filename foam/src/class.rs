ac0b6d081f40d8c3a9d18e77dbfcb5c0fa1a96d6
xuerongNanopay <xuerong@nanopay.net>
2025-03-09 00:45:50 -0500

    add struct name

--- foam/src/class.rs
+++ foam/src/class.rs
@@ -1,31 +1,66 @@
 #![allow(unused)]
 
 use proc_macro2::TokenStream;
-use quote::quote;
-use syn::parse::ParseStream;
+use quote::{quote, ToTokens};
+use syn::{parse::ParseStream, Ident, LitStr};
 
-pub(crate) struct ClassParser {
-    pub(crate) name: String,
+mod class_token {
+    syn::custom_keyword!(name);
+}
+
+pub(crate) struct StructParser {
+    struct_name: StructName,
     // pub(crate) table_name: String,
 }
 
-impl syn::parse::Parse for ClassParser {
+impl syn::parse::Parse for StructParser {
     fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let struct_name = input.parse()?;
         let remaining_tokens: TokenStream = input.parse()?;
-        println!("remaining tokens `{}`", remaining_tokens);
-
-        let name = "111".to_owned();
-        
         Ok(Self{
-            name
+            struct_name
         })
     }
 }
 
-pub(crate) fn expand(parser: ClassParser) -> TokenStream {
+pub(crate) fn expand(parser: StructParser) -> TokenStream {
+    let struct_name = &parser.struct_name;
+
     quote::quote! {
-        struct CCCC {
+        struct #struct_name {
             a: u32,
         }
+    }
+}
+
+pub(crate) struct StructName {
+    name: LitStr,
+    _colon: syn::Token![:],
+    _ident: class_token::name,
+}
+
+impl syn::parse::Parse for StructName {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let _ident = input.parse()?;
+
+        let _colon = input.parse()?;
+        println!("_colon");
+
+        let name: LitStr = input.parse()?;
+        println!("name: {}", name.value());
+
+
+        Ok(Self{
+            name,
+            _colon,
+            _ident,
+        })
+    }
+}
+
+impl ToTokens for StructName {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let struct_name = Ident::new(&self.name.value(), self.name.span());
+        struct_name.to_tokens(tokens);
     }
 }
--- foree_backend/src/main.rs
+++ foree_backend/src/main.rs
@@ -1,3 +1,9 @@
+use foam::foam_class_proc;
+
+foam_class_proc!{
+    name: "aaa"
+}
+
 fn main() {
     println!("Hello, world!");
 }

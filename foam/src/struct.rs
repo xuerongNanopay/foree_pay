cffa634376738a39df86ce05b771996330da94fc
xuerongNanopay <xuerong@nanopay.net>
2025-03-09 13:57:13 -0400

    add use statement parse

--- foam/src/struct.rs
+++ foam/src/struct.rs
@@ -2,10 +2,11 @@
 
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse::ParseStream, Ident, LitStr};
+use syn::{bracketed, parse::ParseStream, punctuated::Punctuated, Ident, LitStr, Token};
 
 mod class_token {
     syn::custom_keyword!(struct_name);
+    syn::custom_keyword!(imports);
 }
 
 pub(crate) struct StructParser {
@@ -40,19 +41,19 @@ pub(crate) struct Features {
 pub(crate) struct StructName {
     name: LitStr,
     _colon: syn::Token![:],
-    _ident: class_token::struct_name,
+    _key: class_token::struct_name,
 }
 
 impl syn::parse::Parse for StructName {
     fn parse(input: ParseStream) -> Result<Self, syn::Error> {
-        let _ident = input.parse()?;
+        let _key = input.parse()?;
         let _colon = input.parse()?;
         let name: LitStr = input.parse()?;
 
         Ok(Self{
             name,
             _colon,
-            _ident,
+            _key,
         })
     }
 }
@@ -61,5 +62,29 @@ impl ToTokens for StructName {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let struct_name = Ident::new(&self.name.value(), self.name.span());
         struct_name.to_tokens(tokens);
+    }
+}
+
+pub(crate) struct UseStatements {
+    _key: class_token::imports,
+    _colon: syn::Token![:],
+    use_statements: Punctuated<LitStr, Token![,]>
+}
+
+impl syn::parse::Parse for UseStatements {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let _key = input.parse()?;
+        let _colon = input.parse()?;
+
+        let content;
+        bracketed!(content in input);
+
+        let use_statements = Punctuated::parse_terminated(&content)?;
+
+        Ok(Self{
+            use_statements,
+            _colon,
+            _key,
+        })
     }
 }

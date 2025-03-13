8607b1322a088eaa8a6ca50623eada89f760273c
xuerongNanopay <xuerong@nanopay.net>
2025-03-12 20:49:07 -0400

    add properties

--- nano_foam/src/property.rs
+++ nano_foam/src/property.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use syn::{punctuated::Punctuated, Token};
+use syn::{parse::ParseStream, punctuated::Punctuated, LitStr, Token};
 
 use crate::token;
 
@@ -12,5 +12,55 @@ pub(super) struct Properties {
 // }
 
 pub(super) struct Property {
+    property_name: Option<PropertyName>,
+    property_class: Option<PropertyClass>,
+    property_of: Option<PropertyOf>
+}
 
+pub(crate) struct PropertyName {
+    name: LitStr,
+}
+
+impl syn::parse::Parse for PropertyName {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        input.parse::<token::name>()?;
+        input.parse::<syn::Token![:]>()?;
+        let name: LitStr = input.parse()?;
+
+        Ok(Self{
+            name,
+        })
+    }
+}
+
+pub(crate) struct PropertyClass {
+    class: LitStr,
+}
+
+impl syn::parse::Parse for PropertyClass {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        input.parse::<token::class>()?;
+        input.parse::<syn::Token![:]>()?;
+        let class: LitStr = input.parse()?;
+
+        Ok(Self{
+            class,
+        })
+    }
+}
+
+pub(crate) struct PropertyOf {
+    of: LitStr,
+}
+
+impl syn::parse::Parse for PropertyOf {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        input.parse::<token::class>()?;
+        input.parse::<syn::Token![:]>()?;
+        let of: LitStr = input.parse()?;
+
+        Ok(Self{
+            of,
+        })
+    }
 }
--- nano_foam/src/token.rs
+++ nano_foam/src/token.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 
 syn::custom_keyword!(name);
+syn::custom_keyword!(class);
+syn::custom_keyword!(of);
 syn::custom_keyword!(use_imports);
 syn::custom_keyword!(features);
 syn::custom_keyword!(properties);
